f2b0257d414c3b70879a1ec7397855fdbb019095
Miraitowa <x709500@163.com>
2022-12-28 13:29:20 +0800

    修复已知问题

--- .history/src/api/other_20220703085558.js
+++
@@ -1,3 +0,0 @@
-// 支付二维码图片地址
-// export const palImg = "http://192.168.0.106:3303/static/images/pay.png"
-export const palImg = "https://api.helloxlj.top/public/static/file165680971959065391023.png"
--- .history/src/api/other_20221003153054.js
+++
@@ -1,2 +0,0 @@
-// 支付二维码图片地址
-export const palImg = "https://api.helloxlj.top/public/static/file1662721153697790690611.png"
